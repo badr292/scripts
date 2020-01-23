a8ad7f24bb0c8f0fb1343ff699c1c4bff8f8d252
badr292 <A7bnafsy!>
2020-01-23 21:59:53 +0300

    Update Sever side to be able to hunt Flag 00ff

--- udp/server.py
+++ udp/server.py
@@ -1,18 +1,27 @@
-import socket 
+import socket
+
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
-   
+hunt_flag = 0
 while True:
-	data,addr = sock.recvfrom(1024)
-	#data,addr = server_socket.recvfrom(1024)
-	nOb = 0
-	#def utf8len(data):
-	nOb= len(data)
-	lenght= int(data[0]) 
-	if lenght != nOb:
-		print ("Message is not correct!")
-	print ("received message:", data)
-	print ("received message:", data[0])
-	print ("received message:", nOb)
+    data, addr = sock.recvfrom(1024)
+    # data,addr = server_socket.recvfrom(1024)
+
+    while data[hunt_flag] != b'\x00\xFF' and hunt_flag != 1024:
+        hunt_flag+=1
+
+    if hunt_flag == 1024:
+       continue
+    if data[hunt_flag] == b'\x00\xFF':
+        print("Receive Quick packet")
+    nOb = 0
+    # def utf8len(data):
+    nOb = len(data)
+    lenght = int(data[0])
+    if lenght != nOb:
+        print("Message is not correct!")
+    print("received message:", data)
+    print("received message:", data[0])
+    print("received message:", nOb)
